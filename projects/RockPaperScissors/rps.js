f6e0e20af02697f8dc7502c706238601caa7ea00
Alwin Ray <alwinray37@gmail.com>
2022-11-25 21:53:55 -0800

    added the lesson notes into a folder separated by js files

--- projects/RockPaperScissors/rps.js
+++ projects/RockPaperScissors/rps.js
@@ -21,16 +21,18 @@ let getCompChoice = () =>  {
 
 // initializing variables
 let win;
-let score = 0;
-let round = 0;
-let rnd = document.getElementById('rnd');
+let playerScore = 0;
+let compScore = 0;
+let round = 1;
+const rnd = document.getElementById('rnd');
+const rpsGame = document.getElementsByClassName("rps-game");
 
 // function that plays one round of RPS
 function playRound(userChoice) {
     let compChoice = getCompChoice();
     let roundResult = result(userChoice, compChoice);
-    console.log(`You chose ${userChoice}, computer chose ${compChoice}, result: ${roundResult}`)
-    rnd.append(`${round + 1}`)
+    console.log(`You chose ${userChoice}, computer chose ${compChoice}, result: ${roundResult}`);
+    rnd.innerHTML =`Round: ${round}`;
 };
 
 // function for the user choice
@@ -42,12 +44,27 @@ let result = (userChoice, compChoice) => {
     }
     else if(userChoice == 'rock'){
         result = compChoice == 'scissors' ? 'win' : 'lose';
+        if(result =='win'){
+            playerScore++;
+        }else{
+            compScore++;
+        }
     }
     else if (userChoice == 'paper') {
         result = compChoice == 'rock' ? 'win' : 'lose';
+        if(result =='win'){
+            playerScore++;
+        }else{
+            compScore++;
+        }
     }
     else if (userChoice == 'scissors') {
         result = compChoice == 'paper' ? 'win' : 'lose';
+        if(result =='win'){
+            playerScore++;
+        }else{
+            compScore++;
+        }
     }
     // finish round and return result
     round++;
@@ -59,32 +76,43 @@ let result = (userChoice, compChoice) => {
 let setHandlers = () => {
 
 document.getElementById('rock').onclick = function () {
-    if(round < 5){
+    if(compScore < 2){
         playRound('rock');
     }
     else{
-        rnd.style.display = "none";
+       endGame();
     }
 };
 document.getElementById('paper').onclick = function () {
-    if(round < 5){
+    if(compScore < 2){
         playRound('paper');
     }
     else{
-        rnd.style.display = "none";
+        endGame();
     }
 };
 document.getElementById('scissors').onclick = function () {
-    if(round < 5){
+    if(compScore < 2){
         playRound('scissors');
     }
     else{
-        rnd.style.display = "none";
+        endGame();
     }
 };
 };
 
+// Function when game ends
+let endGame =() => {
+    rpsGame.style.display = 'none';
+    if(playerScore == 2){
+        alert("Congrats! You Win!");
+    }else{
+        alert("Sorry you lost :(");
+    }
+};
+
 const main = () => {
     setHandlers();
+    rpsGame.style.cssText = 'color: red;';
 }
 main();
